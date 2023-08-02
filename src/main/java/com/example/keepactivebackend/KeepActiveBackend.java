7c8002a2263bad7ef7b65d9f69989b21bfae7090
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-08-02 23:06:57 +0300

    refactor: create tables if they don't exist

--- src/main/java/com/example/keepactivebackend/KeepActiveBackend.java
+++ src/main/java/com/example/keepactivebackend/KeepActiveBackend.java
@@ -1,43 +1,11 @@
 package com.example.keepactivebackend;
 
-import com.example.keepactivebackend.auth.AuthenticationService;
-import com.example.keepactivebackend.auth.SignUpRequest;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import static com.example.keepactivebackend.user.Role.ADMIN;
-import static com.example.keepactivebackend.user.Role.MANAGER;
 
 @SpringBootApplication
 public class KeepActiveBackend {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KeepActiveBackend.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(
-			AuthenticationService service
-	) {
-		return args -> {
-			var admin = SignUpRequest.builder()
-					.username("Admin")
-					.email("admin@mail.com")
-					.password("password")
-					.role(ADMIN)
-					.build();
-			System.out.println("Admin token: " + service.signup(admin).getAccessToken());
-
-			var manager = SignUpRequest.builder()
-					.username("Admin")
-					.email("manager@mail.com")
-					.password("password")
-					.role(MANAGER)
-					.build();
-			System.out.println("Manager token: " + service.signup(manager).getAccessToken());
-
-		};
 	}
 }
